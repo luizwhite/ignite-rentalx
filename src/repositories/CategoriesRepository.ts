00c8c767ea71a176cfffdf0dc09f84d2fdef9b27
Luiz Augusto <luiz.a.marques@kroton.com.br>
2021-07-11 22:31:56 -0300

    Chapter II - L principle

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,18 +1,17 @@
 import { Category } from '../model/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from './ICategoriesRepository';
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[] = [];
 
   constructor() {
     this.categories = [];
   }
 
-  create: (data: ICreateCategoryDTO) => void = ({ name, description }) => {
+  create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
     Object.assign(category, {
       name,
@@ -20,17 +19,19 @@ class CategoriesRepository {
     });
 
     this.categories.push(category);
-  };
+  }
 
-  list: () => Category[] = () => this.categories;
+  list(): Category[] {
+    return this.categories;
+  }
 
-  findByName: (name: string) => Category | null = (name) => {
+  findByName(name: string): Category | null {
     const category = this.categories.find(
       ({ name: catName }) => catName === name
     );
 
     return category || null;
-  };
+  }
 }
 
 export { CategoriesRepository };
