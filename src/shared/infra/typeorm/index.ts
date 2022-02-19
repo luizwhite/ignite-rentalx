91811682a046a65e47af61e67ced454b1a9c18cd
Luiz Augusto <luiz.a.marques@kroton.com.br>
2022-02-19 14:00:10 -0300

    :sparkles: Chapter VI - 4th class

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,33 +1,30 @@
 import {
   createConnection as createConnectionBase,
   getConnectionOptions,
-  // getConnectionManager,
 } from 'typeorm';
 import { Connection } from 'typeorm/connection/Connection';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-// import { ConnectionManager } from 'typeorm/connection/ConnectionManager';
 
 type createConnectionPostgres = (
   options: PostgresConnectionOptions
 ) => Promise<Connection>;
 const createConnection: createConnectionPostgres = createConnectionBase;
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (
+  host = process.env.PSQL_HOST || 'localhost'
+): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  console.log({ defaultOptions });
 
   const newOptions = {
     ...defaultOptions,
+    port: process.env.PSQL_PORT,
+    password: process.env.PSQL_PASSWORD,
     host: process.env.NODE_ENV !== 'test' ? host : 'localhost',
     database:
       process.env.NODE_ENV !== 'test' ? defaultOptions.database : 'rentx_test',
   } as PostgresConnectionOptions;
 
+  console.log(newOptions);
+
   return createConnection(newOptions);
-
-  // const connectionManager = getConnectionManager();
-  // const connection = connectionManager.create(newOptions);
-  // await connection.connect();
-
-  // return connectionManager;
 };
